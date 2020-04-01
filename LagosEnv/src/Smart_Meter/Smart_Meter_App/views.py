1ea988cc7c5740fb26601ecded45458c293f0c4d
Akinola <makindeakinola22@gmail.com>
2020-04-01 19:17:11 +0100

    Everything is configured appropriately!

--- LagosEnv/src/Smart_Meter/Smart_Meter_App/views.py
+++ LagosEnv/src/Smart_Meter/Smart_Meter_App/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+
+# These view is to test that everything has been condigured appropriately
+
+
+def index(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
