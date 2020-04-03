9d4cb14087675a6df112a04525ec87e510be3e0e
Akinola <makindeakinola22@gmail.com>
2020-04-03 02:28:55 +0100

    few modifications

--- LagosEnv/src/Smart_Meter/Smart_Meter_App/views.py
+++ LagosEnv/src/Smart_Meter/Smart_Meter_App/views.py
@@ -8,6 +8,4 @@ import datetime
 
 
 def index(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    return render(request, 'index.html', {})
