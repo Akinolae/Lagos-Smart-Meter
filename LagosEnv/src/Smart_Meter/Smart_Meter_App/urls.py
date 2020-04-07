72e3bb66bee6409bb99e6b8c877416e4f52898f8
Akinola <makindeakinola22@gmail.com>
2020-04-07 10:34:00 +0100

    added the logout feature

--- LagosEnv/src/Smart_Meter/Smart_Meter_App/urls.py
+++ LagosEnv/src/Smart_Meter/Smart_Meter_App/urls.py
@@ -3,7 +3,7 @@ from Smart_Meter_App.views import index
 # from Stakeholder_Dashboard.views import stakeholder
 from Administrative_Dashboard.views import admin_page, about
 
-from administrator.views import register, login, logout, dashboard, administrator_index
+from administrator.views import register, dashboard, administrator_index
 
 from Subscriber_Dashboard.views import subscriber_page
 
@@ -11,10 +11,7 @@ from Subscriber_Dashboard.views import subscriber_page
 urlpatterns = [
     path('', administrator_index, name='adminIndex'),
     path('subscriber/', subscriber_page, name='subscriber'),
-    # path('administrator/', administrator_index, name='adminIndex'),
     path('administrator/about', about, name='admin about'),
-    path('administrator/login', login, name='login'),
-    path('administrator/logout', logout, name='logout'),
     path('administrator/dashboard', dashboard, name='dashboard'),
     path('administrator/register', register, name='register'),
     path('subscriber/', subscriber_page, name='subscriber'),
---
+++ LagosEnv/src/Smart_Meter/administrator/forms.py
@@ -0,0 +1,1 @@
+from cloudinary.forms import CloudinaryFileField
